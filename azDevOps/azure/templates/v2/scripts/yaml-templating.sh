d8c58ed65697774962d456f42dded635ea2470e7
Dusan Nitschneider <nitschneiderd@gmail.com>
2020-04-08 11:16:02 +0100

    fix: cleaned up templating task and aux script

--- azDevOps/azure/templates/v2/scripts/yaml-templating.sh
+++ azDevOps/azure/templates/v2/scripts/yaml-templating.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
-# arg1=templat-in.yml
-# arg2=templat-out.yml
+
 base_template="$1"
-out_template="$2"
-# remove template-in if exists
-rm -f "$out_template"
+file_out="$2"
+templated_out_yaml=""
+
 # remove temp if exists
 rm -f temp.yml
-( echo "cat <<EOF >$out_template";
-  cat "$base_template";
-  echo "EOF";
-) >temp.yml
-. temp.yml
-rm -f temp.yml
-cat $out_template
+
+# :param string: base_yaml path
+# :param string: out_yaml path
+# :return string - out path of the templated yaml file
+function do_templating() {
+  local base_yaml="$1"
+  local out_yaml="$2"
+  ( echo "cat <<EOF >$out_yaml";
+    cat "$base_yaml";
+    echo "EOF";
+  ) >temp.yml
+  . temp.yml
+  rm -f temp.yml
+  # returns a string
+  echo $out_yaml
+  return 0
+}
+
+if [ -z "$file_out" ]; then
+  echo """
+  out not supplied - defaulting to stripping base_ from base yaml
+  Ensure you are following conventions and prepend you base yaml with `base_`
+  """
+  out_template="${base_template//"base_"}"
+  rm -f $out_template
+  echo "generated output path for the templated file: $out_template"
+else
+  # remove template-in if exists
+  rm -f "$file_out"
+  out_template="$file_out"
+  echo "supplied output path for the templated file: $out_template"
+fi
+
+echo "base yaml: $base_template"
+echo "out_template yaml: $out_template"
+
+ret_val="$(do_templating $base_template $out_template)"
+
+cat $ret_val
+exit 0;
