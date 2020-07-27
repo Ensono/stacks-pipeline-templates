72f9d1a551424f1488aa85547d1b4807948eb3fd
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-07-27 11:41:13 +0100

    feat: more scripts, new design for them

--- scripts/test-maven-tagged-test-run.bash
+++ scripts/test-maven-tagged-test-run.bash
@@ -2,19 +2,54 @@
 
 set -exo pipefail
 
-# Required
-GROUP="$1"
+usage()
+{
+	set +x
+	USAGE=$(cat <<- USAGE_STRING
+		Usage: $(basename $0) [OPTION]...
 
-# Optional
-M2_LOCATION="$2"
+		Required Arguments:
+		  -a Tag	The test tag to run.
 
-if [ -z "$GROUP" ]; then
-	echo 'Please specify a group of tests to run, e.g. `Unit`.'
+		Optional Arguments:
+		  -Z location	Optional maven cache directory. Default: \`./.m2\`
+	USAGE_STRING
+	)
+
+	echo "${USAGE}"
+
+	set -x
+}
+
+# Detect `--help`, show usage and exit.
+for var in "$@"; do
+	if [ "${var}" == '--help' ]; then
+		usage
+		exit 0
+	fi
+done
+
+while getopts "${OPTIONS}" option
+do
+	case "${option}" in
+		# Required
+		a  ) GROUP="${OPTARG}";;
+		# Optional
+		Z  ) M2_LOCATION="${OPTARG}";;
+
+		\? ) echo "Unknown option: -${OPTARG}" >&2; exit 1;;
+		:  ) echo "Missing option argument for -${OPTARG}" >&2; exit 1;;
+		*  ) echo "Unimplemented option: -${option}. This is probably unintended." >&2; exit 1;;
+	esac
+done
+
+if [ -z "${GROUP}" ]; then
+	echo '-a: Missing a group of tests to run, e.g. `Unit`.'
 	exit 1
 fi
 
-if [ -z "$M2_LOCATION" ]; then
+if [ -z "${M2_LOCATION}" ]; then
 	M2_LOCATION="./.m2"
 fi
 
-./mvnw test --no-transfer-progress -Dmaven.repo.local="$M2_LOCATION" --offline -Dgroups="$GROUP"
+./mvnw test --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" --offline -Dgroups="${GROUP}"
