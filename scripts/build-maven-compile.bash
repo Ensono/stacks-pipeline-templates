742e78eb42b13bd74a392a9f1d37fd80cefc5b42
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-08-05 10:48:50 +0100

    feat: Adds untagged test check for post deploy

--- scripts/build-maven-compile.bash
+++ scripts/build-maven-compile.bash
@@ -47,6 +47,7 @@ if [ -z "${M2_LOCATION}" ]; then
 	M2_LOCATION="./.m2"
 fi
 
-./mvnw compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" --offline
+./mvnw compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" # TODO: Maybe this should run offline?
 ./mvnw process-test-resources --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" --offline
-./mvnw test-compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" # TODO: Maybe this should be run offline?
+./mvnw test-compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}"
+./mvnw process-test-classes --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" # TODO: Maybe this should be run offline?
