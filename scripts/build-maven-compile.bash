4bc522c76ecce917f70d348ac836171ff6b81ffc
guillermo <guillonrg@gmail.com>
2021-11-09 10:14:35 +0000

    Remove --offline option to avoid issues with missing dependencies

--- scripts/build-maven-compile.bash
+++ scripts/build-maven-compile.bash
@@ -47,7 +47,12 @@ if [ -z "${M2_LOCATION}" ]; then
 	M2_LOCATION="./.m2"
 fi
 
+#./mvnw compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" # TODO: Maybe this should run offline?
+#./mvnw process-test-resources --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" --offline
+#./mvnw test-compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}"
+#./mvnw process-test-classes --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" # TODO: Maybe this should be run offline?
+
 ./mvnw compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" # TODO: Maybe this should run offline?
-./mvnw process-test-resources --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" --offline
+./mvnw process-test-resources --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}"
 ./mvnw test-compile --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}"
 ./mvnw process-test-classes --no-transfer-progress -Dmaven.repo.local="${M2_LOCATION}" # TODO: Maybe this should be run offline?
