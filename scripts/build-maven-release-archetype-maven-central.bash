0347a78322319f2e4149d1a592a1808631c0e65a
Mohamed Samandi <mohamedsamandi@AML0350.local>
2022-03-15 10:19:09 +0100

    refactoring code

--- scripts/build-maven-release-archetype-maven-central.bash
+++ scripts/build-maven-release-archetype-maven-central.bash
@@ -87,6 +87,5 @@ if [ "${ALT_DEPLOYMENT_REPOSITORY}" ]; then
 	MAVEN_OPTIONS+=" -DaltDeploymentRepository=${ALT_DEPLOYMENT_REPOSITORY} "
 fi
 
-./mvnw release:clean
 ./mvnw --batch-mode release:prepare ${MAVEN_OPTIONS}
 ./mvnw release:perform  ${MAVEN_OPTIONS}
