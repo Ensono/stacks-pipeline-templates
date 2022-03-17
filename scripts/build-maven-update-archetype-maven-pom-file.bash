d34a881ff3fbccdf5c9c7f29f97c14557180ba8d
Mohamed Samandi <mohamedsamandi@AML0350.local>
2022-03-17 15:22:25 +0100

    update archetype pom file

--- scripts/build-maven-update-archetype-maven-pom-file.bash
+++ scripts/build-maven-update-archetype-maven-pom-file.bash
@@ -7,4 +7,3 @@ set -exo pipefail
 cd target/generated-sources/archetype
 
 sed -i -e "s/<build>/<build><plugins><plugin><groupId>org.sonatype.plugins<\/groupId><artifactId>nexus-staging-maven-plugin<\/artifactId><version>1.6.7<\/version><extensions>true<\/extensions><configuration><serverId>ossrh<\/serverId><nexusUrl>https:\/\/s01.oss.sonatype.org\/<\/nexusUrl><autoReleaseAfterClose>false<\/autoReleaseAfterClose><\/configuration><\/plugin><plugin><groupId>org.apache.maven.plugins<\/groupId><artifactId>maven-gpg-plugin<\/artifactId><version>1.5<\/version><executions><execution><id>sign-artifacts<\/id><phase>verify<\/phase><goals><goal>sign<\/goal><\/goals><\/execution><\/executions><\/plugin><\/plugins>/g" pom.xml
-cat pom.xml
