88b4061f15944b140c45aa0b89472f0c26dce4c7
Mohamed Samandi <mohamedsamandi@AML0350.local>
2022-03-29 17:29:47 +0100

    update parent missing distributionmanagenemnt in archtype pom file

--- scripts/build-maven-update-archetype-maven-pom-file.bash
+++ scripts/build-maven-update-archetype-maven-pom-file.bash
@@ -7,7 +7,12 @@ set -exo pipefail
 
 cd target/generated-sources/archetype
 
-if grep -Fxq "distributionManagement"   ${0}
-then sed -i -e "s/<scm>/<distributionManagement><snapshotRepository><id>ossrh<\/id><url>https:\/\/s01.oss.sonatype.org\/content\/repositories\/snapshots<\/url><\/snapshotRepository><repository><id>ossrh<\/id><url>https:\/\/s01.sonatype.org\/service\/local\/staging\/deploy\/maven2\/<\/url><\/repository><\/distributionManagement><scm>/g" pom.xml
+isInFile=$(cat pom.xml | grep -c "distributionManagement")
+
+
+if [[ $isInFile -eq 0 ]]
+then
+  sed -i -e "s/<scm>/<distributionManagement><snapshotRepository><id>ossrh<\/id><url>https:\/\/s01.oss.sonatype.org\/content\/repositories\/snapshots<\/url><\/snapshotRepository><repository><id>ossrh<\/id><url>https:\/\/s01.sonatype.org\/service\/local\/staging\/deploy\/maven2\/<\/url><\/repository><\/distributionManagement><scm>/g" pom.xml
+fi
 
 sed -i -e "s/<build>/<build><plugins><plugin><groupId>org.sonatype.plugins<\/groupId><artifactId>nexus-staging-maven-plugin<\/artifactId><version>1.6.7<\/version><extensions>true<\/extensions><configuration><serverId>ossrh<\/serverId><nexusUrl>https:\/\/s01.oss.sonatype.org\/<\/nexusUrl><autoReleaseAfterClose>false<\/autoReleaseAfterClose><\/configuration><\/plugin><plugin><groupId>org.apache.maven.plugins<\/groupId><artifactId>maven-gpg-plugin<\/artifactId><version>1.5<\/version><executions><execution><id>sign-artifacts<\/id><phase>verify<\/phase><goals><goal>sign<\/goal><\/goals><\/execution><\/executions><\/plugin><\/plugins>/g" pom.xml
