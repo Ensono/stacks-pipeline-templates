9d833dc8e1352b7f6555c761956700ca60157267
Mohamed Samandi <mohamedsamandi@AML0350.local>
2022-10-11 18:17:37 +0100

    fix install manifold for dynamo project

--- scripts/test-maven-post-deploy-tagged-test-run.bash
+++ scripts/test-maven-post-deploy-tagged-test-run.bash
@@ -78,6 +78,9 @@ echo $BASE_URL
 env |grep BASE_URL
  aws dynamodb list-tables
 aws dynamodb list-tables --endpoint-url https://dynamodb.eu-west-2.amazonaws.com
+aws dynamodb list-tables --endpoint-url http://dynamodb.eu-west-2.amazonaws.com
+aws dynamodb list-tables --endpoint-url h
+
 
 
 ./mvnw failsafe:integration-test \
