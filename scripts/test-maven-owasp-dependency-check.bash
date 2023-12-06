b1f88a5e9f566c70bba19061e934757816678c36
Jack Blower <Jack@elvenspellmaker.co.uk>
2023-12-06 15:36:06 +0000

    feat: Allows NVD API Key to be specified (OWASP)

--- scripts/test-maven-owasp-dependency-check.bash
+++ scripts/test-maven-owasp-dependency-check.bash
@@ -4,7 +4,7 @@
 
 set -exo pipefail
 
-OPTIONS=":Y:Z:"
+OPTIONS=":X:Y:Z:"
 
 usage()
 {
@@ -13,6 +13,7 @@ usage()
 		Usage: $(basename "${0}") [OPTION]...
 
 		Optional Arguments:
+		  -X key	An API key for the NVD Databases (NOTE: This will be very slow without one, they can be requested here: https://nvd.nist.gov/developers/request-an-api-key)
 		  -Y true|false	Whether to fail the build or not on vulnerabilities. Default: false
 		  -Z location	Optional maven cache directory. Default: './.m2'
 		USAGE_STRING
@@ -35,6 +36,7 @@ while getopts "${OPTIONS}" option
 do
 	case "${option}" in
 		# Optional
+		X  ) NVD_API_KEY="${OPTARG}";;
 		Y  ) FAIL_BUILD_ON_VULNERABILITY="${OPTARG}";;
 		Z  ) M2_LOCATION="${OPTARG}";;
 
@@ -44,12 +46,18 @@ do
 	esac
 done
 
+declare -a EXTRA_OWASP_ARGUMENTS
+
 if [ -z "${M2_LOCATION}" ]; then
 	M2_LOCATION="./.m2"
 fi
 
 if [ -z "${FAIL_BUILD_ON_VULNERABILITY}" ]; then
 	FAIL_BUILD_ON_VULNERABILITY="false"
+fi
+
+if [ ! -z "${NVD_API_KEY}" ]; then
+	EXTRA_OWASP_ARGUMENTS+=("-DnvdApiKey=${NVD_API_KEY}")
 fi
 
 FAIL_BUILD_ON_VULNERABILITY="$(tr '[:upper:]' '[:lower:]' <<< "${FAIL_BUILD_ON_VULNERABILITY}")"
@@ -63,4 +71,5 @@ fi
 	-Dmaven.repo.local="${M2_LOCATION}" \
 	-Dsun.jnu.encoding=UTF-8 \
 	-Dfile.encoding=UTF-8 \
-	-DfailBuildOnAnyVulnerability="${FAIL_BUILD_ON_VULNERABILITY}"
+	-DfailBuildOnAnyVulnerability="${FAIL_BUILD_ON_VULNERABILITY}" \
+	"${EXTRA_OWASP_ARGUMENTS[@]}"
