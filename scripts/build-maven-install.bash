c0d59c271ef051599d1de4d034ef79d90951fdd8
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-10-04 18:16:35 +0100

    fix: archetype fixes for maven central

--- scripts/build-maven-install.bash
+++ scripts/build-maven-install.bash
@@ -4,7 +4,7 @@
 
 set -exo pipefail
 
-OPTIONS=":Z:U:P:S:"
+OPTIONS=":Z:"
 
 usage()
 {
@@ -13,9 +13,6 @@ usage()
 		Usage: $(basename "${0}") [OPTION]...
 
 		Optional Arguments:
-		  -U location	Optional username for the repository
-		  -P location	Optional password for the repository
-		  -S location	Optional maven settings file. Default: \`./.mvn/settings.xml\`
 		  -Z location	Optional maven cache directory. Default: \`./.m2\`
 		USAGE_STRING
 	)
@@ -38,9 +35,6 @@ do
 	case "${option}" in
 		# Optional
 		Z  ) M2_LOCATION="${OPTARG}";;
-	  U  ) ARTIFACTORY_USER="${OPTARG}";;
-	  P  ) ARTIFACTORY_PASSWORD="${OPTARG}";;
-    S  ) SETTINGS_LOCATION="${OPTARG}";;
 
 		\? ) echo "Unknown option: -${OPTARG}" >&2; exit 1;;
 		:  ) echo "Missing option argument for -${OPTARG}" >&2; exit 1;;
@@ -53,14 +47,6 @@ if [ -z "${M2_LOCATION}" ]; then
 fi
 
 MAVEN_OPTIONS=" -Dmaven.repo.local=${M2_LOCATION} --no-transfer-progress "
-
-if [ "${ARTIFACTORY_USER}" ]; then
-  MAVEN_OPTIONS+=" -Dartifactory.username=${ARTIFACTORY_USER} -Dartifactory.password=${ARTIFACTORY_PASSWORD} "
-fi
-
-if [ "${SETTINGS_LOCATION}" ]; then
-  MAVEN_OPTIONS+=" --settings ${SETTINGS_LOCATION} "
-fi
 
 ./mvnw dependency:go-offline ${MAVEN_OPTIONS}
 ./mvnw process-resources ${MAVEN_OPTIONS}
